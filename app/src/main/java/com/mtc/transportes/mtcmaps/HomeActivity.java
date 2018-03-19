7007fe11ea28d4f9b6231e314146644a5d573f51
Raul S. Quispe M <raul.quispe@live.com>
2018-03-19 17:31:47 -0500

    Permisos Mapas

--- app/src/main/java/com/mtc/transportes/mtcmaps/HomeActivity.java
+++ app/src/main/java/com/mtc/transportes/mtcmaps/HomeActivity.java
@@ -1,18 +1,30 @@
 package com.mtc.transportes.mtcmaps;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import API.Model.AddressComponent;
 import API.Model.Districts;
 import API.Model.Result;
 import API.Service.ApiClient;
+
+import java.sql.Connection;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,14 +35,19 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class HomeActivity extends AppCompatActivity {
     TextView textDistrict;
     RecyclerView recyclerView;
+    Button btnMapas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnMapas = findViewById(R.id.btnMaps);
         textDistrict = findViewById(R.id.editTextDistrict);
         recyclerView = findViewById(R.id.rvAddress);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
-    }
+        btnMapas.setVisibility(View.INVISIBLE);
+       // this.getPermmission();
+     }
+
     public void getAddressFromDistrict(View view){
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl(getString(R.string.url_base))
@@ -65,5 +82,42 @@ public class HomeActivity extends AppCompatActivity {
         });
 
     }
-
+    public void goToMaps(View view){
+        Intent intent = new Intent(HomeActivity.this,MapsActivity.class);
+        startActivity(intent);
+    }//com.android.support:support-v4:23.0.0
+    public void getPermmission(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        } else {
+            // permission has been granted, continue as usual
+            btnMapas.setVisibility(View.VISIBLE);
+            btnMapas.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(HomeActivity.this,MapsMTCActivity.class);
+                    HomeActivity.this.startActivity(intent);
+                }
+            });
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        2);
+            } else {
+                // Permission was denied. Display an error message.
+                this.getPermmission();
+            }
+        }
+    }
 }
